04a2727a63e1fc9a150252f30ab9fabf00329ca0
Solomon <sdgoldiamond@gmail.com>
2017-05-26 08:24:54 -0400

    first commit

---
+++ shipyard.js
@@ -0,0 +1,13 @@
+
+function launchpad() {
+  return 'Spaceship is ready to launch!';
+}
+
+import { ourShip, launchpad} from 'Spaceship.js';
+import * as CrewMemmber from 'CrewMemmber.js';
+
+var crewNames = [Keri, Jess, Taylor, Byran, Cohen];
+
+function trainCrew() {
+  
+}
